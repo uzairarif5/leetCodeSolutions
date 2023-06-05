7b55d4eee1044d6a506944332b04a52c697dfc1d
Uzair <uzair.99@hotmail.com>
2023-06-05 03:04:12 -0400

    lps-corrected

--- longest-palindromic-subsequence.cpp
+++ longest-palindromic-subsequence.cpp
@@ -9,43 +9,30 @@ using namespace std;
 class Solution {
 public:
 
-    map<string, int> storedAns;
-
-    ofstream outputFile;
-    void openFile(){
-        this->outputFile.open("output.txt");
-    }
-
-    void closeFile(){
-        this->outputFile.close();
-    }
-
-    bool isPalindrome(string s){
-        if(s.length()==1) return true;
-        if(s.length()==2){
-            if(s[0]==s[1]) return true;
-            else return false;
-        }
-        if(s.length()==3){
-            if(s[0]==s[2]) return true;
-            else return false;
-        }
-        for(int i =0; i<s.length()/2;i++ ){if(s[i] != s[s.length()-i-1]) return false;}
-        return true;
-    }
-
     int longestPalindromeSubseq(string s) {
-        for(int i=0;i<s.length();i++){
-            string secondS = s;
-            this->outputFile << secondS.erase(i,1) << "\n";
+        int subseqMatrix[s.length()][s.length()];
+        for(int i=0; i<s.length(); i++){
+            for(int j=0; j<s.length(); j++){
+                subseqMatrix[i][j] = 0;
+            }
         }
-        return 0;
+        for(int i=0;i<s.length();i++){
+            subseqMatrix[i][i] = 1;
+        }
+        for(int i=s.length()-2;i>=0;i--){
+            if(s[i] == s[i+1]) subseqMatrix[i][i+1] = 2;
+            else subseqMatrix[i][i+1] = 1;
+            for(int j=i+2;j<s.length();j++){
+                if(s[i] == s[j]) subseqMatrix[i][j] = 2 + subseqMatrix[i+1][j-1];
+                else subseqMatrix[i][j] = max(subseqMatrix[i][j-1],subseqMatrix[i+1][j]);
+            }
+        }
+        return subseqMatrix[0][s.length()-1];
     }
 };
 
 int main(){
     Solution sol;
-    sol.openFile();
     cout << sol.longestPalindromeSubseq("abcde")<<endl;
     cout << sol.longestPalindromeSubseq("cbabbgab")<<endl;
     cout << sol.longestPalindromeSubseq("pfffft")<<endl;
@@ -53,134 +40,5 @@ int main(){
     cout << sol.longestPalindromeSubseq("racecar")<<endl;
     cout << sol.longestPalindromeSubseq("abcabcabcabc")<<endl;
     cout << sol.longestPalindromeSubseq("euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew")<<endl;
-    sol.closeFile();
     return 0;
 }
-
-
-
-/*
-
-
-    map<string, int> storedAns;
-
-    ofstream outputFile;
-    void openFile(){
-        this->outputFile.open("output.txt");
-    }
-
-    void closeFile(){
-        this->outputFile.close();
-    }
-
-    bool isPalindrome(string s){
-        if(s.length()==1) return true;
-        if(s.length()==2){
-            if(s[0]==s[1]) return true;
-            else return false;
-        }
-        if(s.length()==3){
-            if(s[0]==s[2]) return true;
-            else return false;
-        }
-        for(int i =0; i<s.length()/2;i++ ){if(s[i] != s[s.length()-i-1]) return false;}
-        return true;
-    }
-
-    bool checkAllOnes(vector<int> arr){
-        for(int i =0; i<arr.size(); i++){if(arr[i] == 0) return false;}
-        return true;
-    }
-
-    int longestPalindromeSubseq(string s) {
-        vector<int> maskArr(s.length(), 0);
-        int maxPalindrome = 1;
-        while(!checkAllOnes(maskArr)){
-            string secondS;
-            for(int i =0; i<s.length(); i++){
-                if(maskArr[i]==0) secondS.push_back(s[i]);
-            }
-            if(maxPalindrome<secondS.length()&&isPalindrome(secondS)) maxPalindrome = secondS.length();
-            int i = s.length()-1;
-            maskArr[i] += 1;
-            while(maskArr[i]==2){
-                maskArr[i] = 0;
-                i--;
-                maskArr[i] += 1;
-            }
-        }
-        return maxPalindrome;
-    }
-*/
-
-/*
-Old code:
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-#include <stdlib.h>
-using namespace std;
-
-class Solution {
-public:
-    map<string, int> storedAns ={};
-
-    ofstream outputFile;
-    void openFile(){
-        this->outputFile.open("output.txt");
-    }
-
-    void closeFile(){
-        this->outputFile.close();
-    }
-
-    bool isPalindrome(string s){
-        for(int i =0; i<s.length()/2;i++ ){if(s[i] != s[s.length()-i-1]) return false;}
-        return true;
-    }
-
-    int longestPalindromeSubseq(string s) {
-        this->outputFile << s << endl;
-        if(s.length()==1) return 1;
-        if(s.length()==2){
-            if(s[0]==s[1]) return 2;
-            else return 1;
-        }
-        if(s.length()==3){
-            if(s[0]==s[2]) return 3;
-            else if(s[0] == s[1] || s[1] == s[2]) return 2;
-            else return 1;
-        }
-        if(this->storedAns.find(s) != this->storedAns.end()) return this->storedAns[s];
-        else if (isPalindrome(s)){
-            this->storedAns.insert(pair<string,int>(s,s.length()));
-            return s.length();
-        }
-        else{
-            int maxPalindrome = 1;
-            for(int i=0;i<s.length();i++){
-                string secondS = s;
-                int potentialMax = longestPalindromeSubseq(secondS.erase(i,1));
-                if (potentialMax > maxPalindrome) maxPalindrome = potentialMax;
-            }
-            this->storedAns.insert(pair<string,int>(s,maxPalindrome));
-            return maxPalindrome;
-        }
-    }
-};
-
-int main(){
-    Solution sol;
-    sol.openFile();
-    cout << sol.longestPalindromeSubseq("abcde")<<endl;
-    cout << sol.longestPalindromeSubseq("bbbab")<<endl;
-    cout << sol.longestPalindromeSubseq("pfffft")<<endl;
-    cout << sol.longestPalindromeSubseq("bandit")<<endl;
-    cout << sol.longestPalindromeSubseq("racecar")<<endl;
-    cout << sol.longestPalindromeSubseq("abcabcabcabc")<<endl;
-    cout << sol.longestPalindromeSubseq("euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew")<<endl;
-    sol.closeFile();
-    return 0;
-}
-*/
